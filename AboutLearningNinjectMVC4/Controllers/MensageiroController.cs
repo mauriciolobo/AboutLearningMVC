f18e8a1aa789d8ada9d047a1bf0c1fe9fd33c407
Mauricio Lobo <mauriciolobo@gmail.com>
2013-01-07 21:51:59 -0200

    Creating a page starting without dependency, just a common user

---
+++ AboutLearningNinjectMVC4/Controllers/MensageiroController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AboutLearningNinjectMVC4.Controllers
+{
+    public class MensageiroController : Controller
+    {        
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Olá mundo.";
+            return View();
+        }
+
+    }
+}
