732655e448d8423d218f45ded7c5eb606b93ae7d
Mauricio Lobo <mauriciolobo@gmail.com>
2013-01-07 22:55:29 -0200

    Now, Im learning about Custom Login. Making user friendly and developer partnership.

---
+++ AboutCustomLogin/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AboutCustomLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username)
+        {
+            FormsAuthentication.SetAuthCookie(username, false);
+            return RedirectToAction("Index");
+        }
+    }
+}
