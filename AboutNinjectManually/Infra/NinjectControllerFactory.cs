9740de72b672d000c58a460cc3c66b684f59d95a
Mauricio Lobo <mauriciolobo@gmail.com>
2013-01-08 13:54:39 -0200

    Basic implemetation of NinjectControllerFactory.

--- AboutNinjectManually/Infra/NinjectControllerFactory.cs
+++ AboutNinjectManually/Infra/NinjectControllerFactory.cs
@@ -1,11 +1,28 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.SessionState;
+using Ninject;
 
 namespace AboutNinjectManually.Infra
 {
     public class NinjectControllerFactory:DefaultControllerFactory
     {
-        
+        private StandardKernel kernel;
+
+        public NinjectControllerFactory()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, System.Type controllerType)
+        {
+            return controllerType == null ? null : (IController) kernel.Get(controllerType);
+        }
     }
 }
